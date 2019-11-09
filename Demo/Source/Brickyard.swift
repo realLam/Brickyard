986539e702be6fde6b61bd758c37e25f9901c26d
realLam <bokor_lam@foxmail.com>
2019-11-09 11:17:08 +0800

    添加缓存获取size，清空缓存

--- Demo/Source/Brickyard.swift
+++ Demo/Source/Brickyard.swift
@@ -40,3 +40,94 @@ extension Brickyard {
         return newDic
     }
 }
+
+// MARK: - utils for Clean Cache
+extension Brickyard {
+    
+    /// 获取缓存大小
+    /// - Parameter completionHandler: 结果回调
+    class func fileSizeOfCache(completionHandler: @escaping (_ size: String) -> ()) {
+        
+        // 取出cache文件夹目录 缓存文件都在这个目录下
+        guard let cachePath = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).first else { return }
+        // 取出文件夹下所有文件数组
+        guard let fileArr = FileManager.default.subpaths(atPath: cachePath) else { return }
+        
+        let manager = FileManager.default
+        
+        // 开启子线程
+        DispatchQueue.global().async {
+            
+            // 快速枚举出所有文件名 计算文件大小
+            var size = 0
+            for file in fileArr {
+                // 把文件名拼接到路径中
+                let path = cachePath + "/\(file)"
+                // 取出文件属性
+                let floder = try! manager.attributesOfItem(atPath: path)
+                
+                // 用元组取出文件大小属性
+                for (key, value) in floder {
+                    // 累加文件大小
+                    if key == FileAttributeKey.size {
+                        size += (value as AnyObject).integerValue
+                    }
+                }
+            }
+            
+            // 换算
+            var str : String = ""
+            var realSize : Int = size
+            if realSize < 1024  {
+                str = str.appendingFormat("%dB", realSize)
+            }else if size > 1024 && size < 1024 *  1024  {
+                realSize = realSize / 1024
+                str = str.appendingFormat("%dKB", realSize)
+            }else if size > 1024 * 1024 && size < 1024 *  1024  * 1024  {
+                realSize = realSize / 1024 / 1024
+                str = str.appendingFormat("%dM", realSize)
+            }
+            
+            // 回到主线程 执行闭包
+            DispatchQueue.main.async(execute: {
+                completionHandler(str)
+            })
+        }
+    }
+    
+    
+    /// 清空缓存
+    /// - Parameter completionHandler: 结果回调
+    class func clearCache(completionHandler: @escaping () -> ()) {
+        
+        // 取出cache文件夹目录 缓存文件都在这个目录下
+        guard let cachePath = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).first else { return }
+        // 取出文件夹下所有文件数组
+        guard let fileArr = FileManager.default.subpaths(atPath: cachePath) else { return }
+        
+        let manager = FileManager.default
+        
+        // 开启子线程
+        DispatchQueue.global().async {
+            
+            // 遍历删除
+            for file in fileArr {
+                
+                let path = cachePath + "/\(file)"
+                
+                if manager.fileExists(atPath: path) {
+                    do {
+                        try manager.removeItem(atPath: path)
+                    } catch {
+                        
+                    }
+                }
+            }
+            
+            //回到主线程 执行闭包
+            DispatchQueue.main.async(execute: {
+                completionHandler()
+            })
+        }
+    }
+}
